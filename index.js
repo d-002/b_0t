a7363e96063c4a8f5009fe70b8f3bcf64a1b2e0a
d-002 <minecraftenjoy@outlook.fr>
2025-08-22 14:05:02 +0200

    set up llm integration

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
-import "dotenv/config";
-import express from "express";
+const env = require("./env.json");
+const { OpenAIChatApi } = require("llm-api");
+const { Client, GatewayIntentBits } = require("discord.js");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+if (env.discord_token == null
+    || env.openai_token == null)
+    throw new Error("Incomplete env variables");
 
-app.listen(PORT, () => {
-    console.log("Listening on port", PORT);
+
+//----- init
+
+const client = new Client(
+    { intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ] });
+const openai = new OpenAIChatApi(
+    { apiKey: env.openai_token },
+    { model: "gpt-4o-mini", contextSize: 2048 }
+);
+
+
+//----- listeners
+
+client.on("messageCreate", async msg => {
+    if (!isAllowed(msg))
+        return;
+
+    await msg.channel.send(await openai.textCompletion(msg.content));
 });
+
+client.on("ready", () => {
+    console.log("ready.");
+});
+
+client.on("error", error => console.log(error));
+
+
+//----- settings
+
+const allowed_channels = [
+    "1278440332064985232" // #testing
+];
+
+//----- utils
+
+function isAllowed(msg) {
+    if (!allowed_channels.includes(msg.channel.id)) return false;
+    if (client.user.id === msg.author.id) return false;
+
+    return true;
+}
+
+
+//----- start client
+
+client.login(env.discord_token);
